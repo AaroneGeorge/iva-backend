49b2301228f77717a3715db0b152be0a3470a7b9
AaroneGeorge <aaroneofficial1879@gmail.com>
2024-05-16 12:15:42 +0530

    new method added

--- main.py
+++ main.py
@@ -1,79 +1,64 @@
-# uvicorn main:app
-# uvicorn main:app --reload
-
-# Main imports
-from fastapi import FastAPI, File, UploadFile, HTTPException
-from fastapi.responses import StreamingResponse
-from fastapi.middleware.cors import CORSMiddleware
-from decouple import config
-import openai
-from pydantic import BaseModel
-
-class Message(BaseModel):
-    message: str
+import json
+import re
+import random_responses
 
 
-
-# Custom function imports
-from functions.openai_requests import get_chat_response, get_chat_response_fd
-from functions.database import store_messages, reset_messages
-
-
-# Get Environment Vars
-openai.organization = config("OPEN_AI_ORG")
-openai.api_key = config("OPEN_AI_KEY")
+# Load JSON data
+def load_json(file):
+    with open(file) as bot_responses:
+        print(f"Loaded '{file}' successfully!")
+        return json.load(bot_responses)
 
 
-# Initiate App
-app = FastAPI()
+# Store JSON data
+response_data = load_json("bot.json")
 
 
-# CORS - Origins
+def get_response(input_string):
+    split_message = re.split(r'\s+|[,;?!.-]\s*', input_string.lower())
+    score_list = []
 
-origins = [
-    "https://dev-guest.inplass.online",
-    "https://qa-guest.inplass.online",
-    "https://uat-guest.inplass.online",
-    "https://guest.inplass.online",
-    "https://inplass.com",
-    "http://localhost:3002",
-    "http://localhost:3000",
-    "http://localhost:3003",
-]
+    # Check all the responses
+    for response in response_data:
+        response_score = 0
+        required_score = 0
+        required_words = response["required_words"]
+
+        # Check if there are any required words
+        if required_words:
+            for word in split_message:
+                if word in required_words:
+                    required_score += 1
+
+        # Amount of required words should match the required score
+        if required_score == len(required_words):
+            # print(required_score == len(required_words))
+            # Check each word the user has typed
+            for word in split_message:
+                # If the word is in the response, add to the score
+                if word in response["user_input"]:
+                    response_score += 1
+
+        # Add score to list
+        score_list.append(response_score)
+        # Debugging: Find the best phrase
+        # print(response_score, response["user_input"])
+
+    # Find the best response and return it if they're not all 0
+    best_response = max(score_list)
+    response_index = score_list.index(best_response)
+
+    # Check if input is empty
+    if input_string == "":
+        return "Please type something so we can chat :("
+
+    # If there is no good response, return a random one.
+    if best_response != 0:
+        return response_data[response_index]["bot_response"]
+
+    return random_responses.random_string()
 
 
-# CORS - Middleware
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
-
-# Check health
-@app.get("/health")
-async def check_health():
-    return {"response": "healthy"}
-
-
-# Reset Conversation
-@app.get("/reset")
-async def reset_conversation():
-    reset_messages()
-    return {"response": "conversation reset"}
-
-
-
-
-@app.post("/chat/")
-async def chat(message: Message):
-    response = get_chat_response(message.message)
-    return {"response": response}
-
-@app.post("/fdchat/")
-async def chat(message: Message):
-    response = get_chat_response_fd(message.message)
-    return {"response": response}
-
+while True:
+    user_input = input("You: ")
+    print("Bot:", get_response(user_input))
